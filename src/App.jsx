2862dc3180fc4b3df1246ea5fc8c824fff5bbeb0
Turzo02 <naimul198650@gmail.com>
2026-02-26 12:18:19 +0600

    Getting Started ...

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>This is TaskFlow Intern Task</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+ //create a login page with react hookform
+const LoginPage = () => {
+  return (
+    <div>
+      <h1>LoginPage Component Works!</h1>
+    </div>
+  );
+};
+
+export default LoginPage;
